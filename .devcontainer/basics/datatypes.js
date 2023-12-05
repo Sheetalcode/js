9e1920c21848d115881f2821821df0dc33c49fab
Sheetalcode <152860728+Sheetalcode@users.noreply.github.com>
2023-12-05 10:08:09 +0000

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,22 @@
+let isLoggedIn = false
+let age = 10
+
+//data types in js
+//1. number => 2 to power 53
+//2. bigint
+//3. string => ""
+//4. boolean => true/false
+//5. null => standalone value
+//6. undefined
+//7. symbol => unique
+//8. object
+
+// how to know the type of value
+
+console.log(typeof "hitesh");
+console.log(typeof 23);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+

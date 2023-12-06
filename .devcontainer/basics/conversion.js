cef4a23cc78f0cdc9620c84646195c10a7aed015
Sheetalcode <152860728+Sheetalcode@users.noreply.github.com>
2023-12-06 09:04:19 +0000

    datatype conversion

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,17 @@
+//let age = "33"
+let age = "sheetal"
+
+console.log(typeof age);
+console.log(typeof (age));
+
+//conversion
+
+let score = Number(age)
+console.log(typeof score);
+console.log(score);
+
+// "33" = 33
+// "33abc" = NaN [not a number]
+// true = 1 ; false = 0
+// "" = false
+// "sheetal" = true

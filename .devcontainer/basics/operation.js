786d73765d8b8215db10b3c9eed14d04f3729962
Sheetalcode <152860728+Sheetalcode@users.noreply.github.com>
2023-12-06 09:38:10 +0000

    operations

---
+++ .devcontainer/basics/operation.js
@@ -0,0 +1,27 @@
+let a = 8
+let negValue = -a
+console.log(negValue);
+
+//console.log(2+2)    add  - 4
+//console.log(2-2)    subtraction - 0
+//console.log(2*2)     multiply - 4
+//console.log(2/2)    divide - 1
+//console.log(2**4)   power - 16
+//console.log(2%8)    percent - 2
+
+
+let str1 = "Sheetal"
+let str2 = " Parihar"
+let str3 = str1+ str2
+console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" +"2");
+//console.log(1 + 2);
+//console.log("1" +2+2);
+//console.log(2+2+"1");
+
+let number = 100;
+number++;
+console.log(number);

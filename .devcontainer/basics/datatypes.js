46d37243cc49bec45e6c5192911fdaa42f54a332
Sheetalcode <152860728+Sheetalcode@users.noreply.github.com>
2023-12-07 10:44:19 +0000

    datatypes example

--- .devcontainer/basics/datatypes.js
+++ .devcontainer/basics/datatypes.js
@@ -1,7 +1,7 @@
 let isLoggedIn = false
 let age = 10
 
-//data types in js
+// primitive data types in js
 //1. number => 2 to power 53
 //2. bigint
 //3. string => ""
@@ -9,7 +9,7 @@ let age = 10
 //5. null => standalone value
 //6. undefined
 //7. symbol => unique
-//8. object
+
 
 // how to know the type of value
 
@@ -20,3 +20,20 @@ console.log(typeof undefined); //undefined
 
 
 
+// non-primitive (reference) datatypes
+
+// arrays , objects , functions
+
+const arr = ["sheetal" , "shaijal" , "raj" , "anushka"];   //arrays
+console.log(arr)
+
+let myObj = {
+name: "sheetal",      //objects
+age:24,
+
+}
+console.log(myObj)
+
+const myFunction = function() {
+    console.log("Hello World")  //functions
+}
